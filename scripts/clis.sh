b8181c9297ca1c7cf0c60edfbbd0fdb5ca40b466
Amber Salome <asalome@vmware.com>
2021-03-09 16:14:26 -0600

    Fix repo resource reference and update bosh-env command

--- scripts/clis.sh
+++ scripts/clis.sh
@@ -13,7 +13,6 @@ sudo mv pivnet /usr/bin/
 
 wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
 echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-# ...then, update your local package index, then finally install the cf CLI
 sudo apt-get update
 sudo apt-get install cf7-cli
 rm om
